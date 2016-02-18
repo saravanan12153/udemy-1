33728320db0e52efa9886c4f353ea9fc7972f8a7
Hasham <hash004@gmail.com>
2016-02-18 17:25:58 +0000

    Finished Score exercise

--- webDevelopment/dom/scoreKeeper/javascript.js
+++ webDevelopment/dom/scoreKeeper/javascript.js
@@ -5,24 +5,31 @@ var inputLimit = document.querySelector("input[type='number']");
 var limit = document.querySelector("#limit").textContent;
 var playerOneScore = 0;
 var playerTwoScore = 0;
+var winner = false;
 
 playerOne.addEventListener("click", function(){
-  playerOneScore++;
-  if(playerOneScore<limit){
-    document.querySelector("#pOneScore").textContent = playerOneScore;
-  } else if(playerOneScore==limit) {
-    document.querySelector("#pOneScore").textContent = playerOneScore;
-    document.querySelector("#pOneScore").style.color = "green";
+  if(!winner){
+    playerOneScore++;
+    if(playerOneScore<limit){
+      document.querySelector("#pOneScore").textContent = playerOneScore;
+    } else if(playerOneScore==limit) {
+      document.querySelector("#pOneScore").textContent = playerOneScore;
+      document.querySelector("#pOneScore").style.color = "green";
+      winner = true;
+    }
   }
 });
 
 playerTwo.addEventListener("click", function(){
-  playerTwoScore++;
-  if(playerTwoScore<limit){
-    document.querySelector("#pTwoScore").textContent = playerTwoScore;
-  } else if(playerTwoScore==limit) {
-    document.querySelector("#pTwoScore").textContent = playerTwoScore;
-    document.querySelector("#pTwoScore").style.color = "green";
+  if(!winner){
+    playerTwoScore++;
+    if(playerTwoScore<limit){
+      document.querySelector("#pTwoScore").textContent = playerTwoScore;
+    } else if(playerTwoScore==limit) {
+      document.querySelector("#pTwoScore").textContent = playerTwoScore;
+      document.querySelector("#pTwoScore").style.color = "green";
+      winner = true;
+    }
   }
 });
 
@@ -30,9 +37,10 @@ reset.addEventListener("click", function(){
   playerOneScore = playerTwoScore = 0;
   document.querySelector("#pOneScore").textContent = document.querySelector("#pTwoScore").textContent = 0;
   document.querySelector("#pOneScore").style.color = document.querySelector("#pTwoScore").style.color = "black";
+  winner = false;
 });
 
 inputLimit.addEventListener("input", function(){
-  document.querySelector("#limit").textContent = limit = inputLimit.value;
+  document.querySelector("#limit").textContent = limit = Number(this.value);
 
 });
